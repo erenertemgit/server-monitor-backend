78e4f6ddc80b38ed9bfb4ece292663136249876c
Eren Ertem <mail.erenertem@gmail.com>
2024-04-07 18:16:12 +0300

    Initial commit

---
+++ src/main/java/edu/erenertem/servermonitor/ServerMonitorApplication.java
@@ -0,0 +1,13 @@
+package edu.erenertem.servermonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/erenertem/servermonitor/ServerMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.erenertem.servermonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
