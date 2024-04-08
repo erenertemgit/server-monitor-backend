ba11bb75f724585ed7fe5acae7a89ff631dbb033
Eren Ertem <mail.erenertem@gmail.com>
2024-04-08 18:25:22 +0300

    adds database configuration

--- src/main/java/edu/erenertem/servermonitor/Application.java
+++ src/main/java/edu/erenertem/servermonitor/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerMonitorApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerMonitorApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
